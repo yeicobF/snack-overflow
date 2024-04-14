e198051eeac43998db1c991e425648251b2a68c0
yeicob <jacobrdz34@gmail.com>
2024-04-13 21:22:21 -0600

    feat: improve responsiveness of ngo page

--- app/ong/page.tsx
+++ app/ong/page.tsx
@@ -11,15 +11,15 @@ const ONG = {
 export default function ConsumerOrdersPage() {
   return (
     <Container>
-      <header className="flex flex-row gap-4 items-center">
+      <header className="flex gap-4 items-center flex-col sm:flex-row">
         <Image
           width={96}
           height={96}
           src={ONG.image}
           alt={`${ONG.name} logo`}
-          className="rounded-lg aspect-square object-cover object-center"
+          className="rounded-lg aspect-video w-full sm:aspect-square object-cover object-center sm:w-[96px] sm:h-[96px]"
         />
-        <div>
+        <div className="w-full">
           <h1 className="text-4xl font-semibold text-slate-100">{ONG.name}</h1>
           <p className="text-slate-200">{ONG.description}</p>
         </div>
