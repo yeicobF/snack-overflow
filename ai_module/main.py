d03d4b6e6f642560bcf12d6e4c70c0aa2f121334
rodrigogelacio <rodrigopottertenis@live.com.mx>
2024-04-14 02:08:33 -0600

    neural_network

---
+++ ai_module/main.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+import keras
+from keras import layers
+
+
+model = keras.Sequential(
+    [
+        layers.Dense(2, activation="relu", name="layer1"),
+        layers.Dense(3, activation="relu", name="layer2"),
+        layers.Dense(4, name="layer3"),
+    ]
+)
+# Call model on a test input
+x = tf.ones((3, 3))
+y = model(x)
+
+
+
+
