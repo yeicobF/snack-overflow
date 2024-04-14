55e13a0745e20611e81909744c8eca112d3af6ea
rodrigogelacio <rodrigopottertenis@live.com.mx>
2024-04-14 03:09:21 -0600

    db connection

--- ai_module/api.py
+++ ai_module/api.py
@@ -4,12 +4,14 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb://localhost:27017/')
-db = client['mydatabase']
+client = MongoClient('mongodb+srv://snack-overflow:SxaL5fSDYSCRO4ja@cluster0.oy0udmz.mongodb.net/')
+db = client['SnackOverflow']
+collection = db['Items']
 
 @app.route('/getRecommendation')
 def get_Request():
     people = request.args.get('people', default='World')
-    kilos_needed = poeple * .5
+    kilos_needed = int(people) * .5
+    documents = list(collection.find())
     
     return jsonify({'message': f'Hello, {people}!'})
