a017ebd6e90c0d8156e79763249dad33811710ca
yeicob <jacobrdz34@gmail.com>
2024-04-13 22:31:53 -0600

    feat: update container constraints

--- components/container.tsx
+++ components/container.tsx
@@ -1,3 +1,15 @@
-export function Container({ children }: { children: React.ReactNode }) {
-  return <div className="mx-auto container max-w-xl py-16">{children}</div>
+import { cn } from "@/lib/utils"
+
+export function Container({
+  children,
+  className,
+}: {
+  children: React.ReactNode
+  className?: string
+}) {
+  return (
+    <div className={cn("mx-auto container max-w-2xl py-16", className)}>
+      {children}
+    </div>
+  )
 }
